02bc1961d8753c3633e976d77d19e6983e4f2fb4
mindeleven <mindeleven@posteo.de>
2024-07-16 13:26:13 +0200

    Accepting incoming connections and handling the client connection

--- ch04_basic_network_applications/proj01_tcp_echo_server/src/main.rs
+++ ch04_basic_network_applications/proj01_tcp_echo_server/src/main.rs
@@ -12,6 +12,46 @@ use std::{
 
 fn main() {
     // bind the server to the local address and port
-    let listener = TcpListener::bind("127.0.0.1::8080").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     println!("Server listening on 127.0.0.1::8080");
+
+    // accept incoming connections
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                // handle the client connection
+                handle_client(&mut stream);
+            },
+            Err(e) => {
+                println!("We've an error: {}", e);
+            }
+        }
+    }
+}
+
+// handle the client connection
+fn handle_client(stream: &mut TcpStream) {
+    // creating a buffer to hold received data
+    let mut buffer = [0; 1024];
+
+    while match stream.read(&mut buffer) {
+        Ok(size) => {
+            // echo the received data back to the client
+            stream.write_all(&buffer[0..size]).unwrap();
+            // continue loop as long as data being received
+            true
+        },
+        Err(_) => {
+            // println!(
+            //     "An error occured, terminating connection with {}",
+            //     stream.peer_addr().unwrap() // // failed to lookup address information: nodename nor servname provided, or not known
+            // );
+            println!(
+                "An error occured, terminating connection"
+            );
+            false // break loop if error occurs
+        }
+    } {} // haven't seen this before ???
+    
+
 }
