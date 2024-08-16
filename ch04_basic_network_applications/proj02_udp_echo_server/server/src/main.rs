b9aaadfda7e3ceec967ac3b37aee033db24e11f9
mindeleven <mindeleven@posteo.de>
2024-08-16 10:26:32 +0200

    Moved server code to /server subdirectory

--- ch04_basic_network_applications/proj02_udp_echo_server/server/src/main.rs
+++ ch04_basic_network_applications/proj02_udp_echo_server/server/src/main.rs
@@ -1,7 +1,10 @@
+// importing the UdpSocket type from std::net
 use std::net::UdpSocket;
 
 fn main() {
+    // creating UdpSocket, binding it to the local address
     // bind to localhost port 8080
+    // server will listen for incoming udp packets on port 8080
     let socket = UdpSocket::bind("127.0.0.1:8080").unwrap();
 
     println!("Echo server listening to port 8080");
@@ -9,10 +12,14 @@ fn main() {
     let mut buf = [0; 1024]; // buffer to store oncoming data
 
     loop {
+        // server enters infinite lopp to listen contiously for incoming packets
+
+        // socket.recv_from -> receives UDP packet, stores it in buf buffer
         // receive data and sender address
+        // returns number of bytes received and source address
         let (num_bytes, src_addr) = socket.recv_from(&mut buf).unwrap();
 
-        // echo the data back to the sender
+        // echo the data back to the sender at src_addr
         socket.send_to(&buf[0..num_bytes], src_addr).unwrap();
     }
 }
