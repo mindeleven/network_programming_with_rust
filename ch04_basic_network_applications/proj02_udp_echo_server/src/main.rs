4d6bd545e04760ddb6759f41b0e426b7771b82c1
mindeleven <mindeleven@posteo.de>
2024-08-16 09:56:14 +0200

    Scaffolding UDP echo server

--- ch04_basic_network_applications/proj02_udp_echo_server/src/main.rs
+++ ch04_basic_network_applications/proj02_udp_echo_server/src/main.rs
@@ -1,3 +1,18 @@
+use std::net::UdpSocket;
+
 fn main() {
-    println!("Hello, world!");
+    // bind to localhost port 8080
+    let socket = UdpSocket::bind("127.0.0.1:8080").unwrap();
+
+    println!("Echo server listening to port 8080");
+
+    let mut buf = [0; 1024]; // buffer to store oncoming data
+
+    loop {
+        // receive data and sender address
+        let (num_bytes, src_addr) = socket.recv_from(&mut buf).unwrap();
+
+        // echo the data back to the sender
+        socket.send_to(&buf[0..num_bytes], src_addr).unwrap();
+    }
 }
