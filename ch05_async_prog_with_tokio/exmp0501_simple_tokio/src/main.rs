96680134fc9fd31e24624bb3d9babbd8e2d18b10
mindeleven <mindeleven@posteo.de>
2024-08-20 14:43:57 +0200

    Explanatory comments on the key advantages of using async Rust for network applications

--- ch05_async_prog_with_tokio/exmp0501_simple_tokio/src/main.rs
+++ ch05_async_prog_with_tokio/exmp0501_simple_tokio/src/main.rs
@@ -2,6 +2,17 @@
 /// multiple connections
 /// example: fetching data from different websites
 /// 
+/// key advantages of using async Rust for network applications:
+/// -> Improved Performance: 
+///    app can handle multiple connections and requests simultaneously w/o blocking main thread
+/// -> Simplified Concurrency:
+///    async/await syntax makes writing readable asynchronous code easier
+/// -> Resource Efficiency:
+///    async I/O avoids need to create multiple threads
+/// -> Enhanced Scalability:
+///    async apps can scale more easily to increased traffic
+/// -> Improved User Experience:
+///    async apps tend to be more responsive with a smoother user experience
 
 use std::time::Duration;
 use tokio::time::sleep;
