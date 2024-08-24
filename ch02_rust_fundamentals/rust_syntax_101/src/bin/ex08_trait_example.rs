e3bce6ebf15e38165a534ec8529d6edd999dc99c
mindeleven <mindeleven@posteo.de>
2024-08-24 09:46:26 +0200

    Rust syntax 101 with making use of polymorphism and generics for traits

--- ch02_rust_fundamentals/rust_syntax_101/src/bin/ex08_trait_example.rs
+++ ch02_rust_fundamentals/rust_syntax_101/src/bin/ex08_trait_example.rs
@@ -7,17 +7,22 @@
 
 // defining a trait
 trait Animal {
-    fn make_sound(&self);
+    fn make_sound(&self) -> String;
     fn eat(&mut self, food: &str);
 }
 
 // implementing the trait
-struct Dog;
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Dog {
+    breed: String,
+    name: String
+}
 
 // to use the trait you need to implement it for specific types
 impl Animal for Dog {
-    fn make_sound(&self) {
-        println!("Wooooofffff!");
+    fn make_sound(&self) -> String {
+        return "Wooooofffff!".to_string();
     }
 
     fn eat(&mut self, food: &str) {
@@ -25,6 +30,28 @@ impl Animal for Dog {
     }
 }
 
+// using traits: polymorphism and generics
+// polymorphism
+//    -> writing code that works with any type the implements a particular trait
+//    -> we don't need to know specific type at compile time
+// example: feed_animal() function that is generic over any type T 
+// that implements the Animal trait
+fn feed_animal<T: Animal>(animal: &mut T, food: &str) {
+    animal.eat(food);
+}
+
 fn main() {
-    println!("Rust's trait system: the secret sauce of reusability and flexibility");
+
+    println!("### Rust's trait system: the secret sauce of reusability and flexibility ###");
+
+    let mut my_dog = Dog {
+        breed: String::from("German Shepherd"),
+        name: String::from("Bismark")
+    };
+
+    dbg!(&my_dog);
+
+    println!("My dog says: {:?}", my_dog.make_sound());
+    feed_animal(&mut my_dog, "sausages");
+
 }
