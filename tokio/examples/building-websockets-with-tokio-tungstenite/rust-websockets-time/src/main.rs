1ff68feda517c7e7ec44fb28d832b0b23adf63d5
mindeleven <mindeleven@posteo.de>
2024-08-27 13:53:35 +0200

    Another try with a tokio example project and how to get started with websockets using rust

---
+++ tokio/examples/building-websockets-with-tokio-tungstenite/rust-websockets-time/src/main.rs
@@ -0,0 +1,19 @@
+/// coding along with Real-Time Rust: Building WebSockets with Tokio Tungstenite by Chris Hay
+/// video tutorial @ https://www.youtube.com/watch?v=p1ES0apOwUw
+/// description from youtube:
+/* " => in this tutorial, chris shows you how to get started with websockets using rust, tokio 
+and tokio tungenstenite.   
+=> we will create a new websocket client in rustlang using tokio and tokio-tungstenite, connect
+it to a public server, and then be able to send and receive messages to a websocket server.
+=> we will also connect up the rust websocket client to an existing ai agent network that chris 
+has been creating (developed in a compeltely different technology, bun).   
+=> we will then convert the application to becoming an automatic time bot on the ai agent network.
+=> at the end of this video, you'll have a deep understanding on how to create web socket clients 
+using rust" */
+/* dependencies: 
+cargo add tokio -F "tokio/full" futures-util tokio-tungstenite -F "tokio-tungstenite/native-tls"
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
