21c2fe98bd05a577acebf2b74fe30b65cf2944c6
mindeleven <mindeleven@posteo.de>
2024-08-28 09:38:33 +0200

    Setting up a tcp listener and accepting a single connection

--- tokio/examples/chat-tokio-stream/src/main.rs
+++ tokio/examples/chat-tokio-stream/src/main.rs
@@ -6,6 +6,26 @@ you to think about concurrent IO, which is the core purpose of Tokio. In this vi
 demonstrates how you can spawn background tasks to manage independent network streams, and 
 use tokio::select! to concurrently poll tasks which require a shared state. 
 " */
-fn main() {
+
+use tokio::net::{ TcpListener, TcpStream };
+
+/// incremental steps to creating a chat sever
+/// 1st step -> making a tcp echo server
+
+#[tokio::main]
+async fn main() {
+    // 1st step -> making a tcp echo server
+    // TcpListener::bind() returns a Future
+    let listener = TcpListener::bind("127.0.0.1:8080")
+        .await // waiting to get the Result out of the Future
+        .unwrap(); // unpacking the Result
+
+    // accepting connections after we got the listener
+    // -> calling accept() on the listener; returns a Future
+    // -> socket which tcp stream & address which is socket address
+    // after we got socket we can connect with `telnet localhost 8080`
+    let (socket, _addr) = listener.accept().await.unwrap();
+    
+
     println!("Hello, world!");
 }
