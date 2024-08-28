942640ea45b228b83bfac997e2f04b42d7820a64
mindeleven <mindeleven@posteo.de>
2024-08-28 09:13:42 +0200

    Another tokio example project for coding along with creating a chat server with async Rust and tokio

---
+++ tokio/examples/chat-tokio-stream/src/main.rs
@@ -0,0 +1,11 @@
+/// coding along with Creating a Chat Server with async Rust and Tokio by Lily Mara
+/// video tutorial @ https://www.youtube.com/watch?v=T2mWg91sx-o
+/// description from youtube:
+/* "Building a chat server is a great way to learn the Tokio library because a chat server forces 
+you to think about concurrent IO, which is the core purpose of Tokio. In this video, Lily 
+demonstrates how you can spawn background tasks to manage independent network streams, and 
+use tokio::select! to concurrently poll tasks which require a shared state. 
+" */
+fn main() {
+    println!("Hello, world!");
+}
